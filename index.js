4af16b70040d651c0f87e6074eedce55ccf4ebf9
josuegamerldc-star <ooopava1@gmail.com>
2026-02-25 18:56:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const TOKEN = process.env.TOKEN;
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GIST_ID = process.env.GIST_ID;
 
+const OWNER_ID = "841709448338472991"; // seu ID
+
 const client = new Client({
   intents: [
     GatewayIntentBits.DirectMessages,
@@ -19,11 +21,13 @@ client.on("ready", () => {
 
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
-  if (message.channel.type !== 1) return;
-
+  if (message.channel.type !== 1) return; // apenas DM
   if (!message.content.startsWith("/ms ")) return;
 
   const texto = message.content.slice(4);
+
+  // buscar owner
+  const owner = await client.users.fetch(OWNER_ID);
 
   try {
     await axios.patch(
@@ -42,11 +46,24 @@ client.on("messageCreate", async (message) => {
       }
     );
 
-    await message.reply(`✅ A mensagem "${texto}" foi enviada com sucesso`);
+    // resposta pra pessoa
+    await message.reply(`✅ A mensagem "${texto}" foi enviada com sucesso!`);
+
+    // log pro owner
+    await owner.send(
+      `📩 O usuário '${message.author.username}' solicitou para mudar o texto para: ${texto}`
+    );
 
   } catch (err) {
     console.error(err.response?.data || err.message);
+
+    // resposta pra pessoa
     await message.reply("❌ Falha ao enviar mensagem.");
+
+    // log pro owner (erro também)
+    await owner.send(
+      `⚠️ O usuário '${message.author.username}' tentou mudar o texto para: ${texto}, mas ocorreu uma falha.`
+    );
   }
 });
 
